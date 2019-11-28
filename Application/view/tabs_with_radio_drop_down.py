c5cb1609b5c67026b1b44bd0d3d42efc6223052e
Tao Huang <tonyhuang0526ubc@gmail.com>
2019-11-28 11:36:24 -0800

    this is the working version

--- Application/view/tabs_with_radio_drop_down.py
+++ Application/view/tabs_with_radio_drop_down.py
@@ -73,3 +73,97 @@ def make_plot():
     alt.themes.enable('mds_special')
     #alt.themes.enable('none') # to return to default
 
+
+####### Lise, you can insert your functions for your plots in the following 
+
+
+# def fun1():
+#     return p1
+
+# def fun2():
+#     return p2
+
+
+app.layout = html.Div([
+    dcc.Tabs(id='tabs', value='tab1', children=[
+    dcc.Tab(label='Fatality rates per billion by airlines', value='tab-1'),
+    dcc.Tab(label='Counts of different categories of incidents', value='tab-2'),
+    ]),
+    html.Div(id='tabs-content-example')
+    
+    ### ADD CONTENT HERE like: html.H1('text'),
+])
+
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs', 'value')])
+
+def render_content(tab):
+
+    if tab == 'tab-1':
+        return html.Div([
+            dcc.RadioItems(
+        options=[
+        {'label': 'First world', 'value': '##### Lise, you can pick your value here'},
+        {'label': 'Non first world', 'value': '##### Lise, you can pick your value here'},
+        {'label': 'Both', 'value': '##### Lise, you can pick your value here'}
+        ],
+        value='##### Lise, this is the default value for radio button'),
+            html.Iframe(
+                    sandbox='allow-scripts',
+                    id='plot',
+                    height='1000',
+                    width='1500',
+                    style={'border-width': '0'},
+                    
+                    # srcDoc =  # Lise's first function call goes here, please to add .to_html() in the end
+        
+                    ),
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            #Insert code for tab2 plot here
+            dcc.Dropdown(
+        id='dd-chart1',
+        options=[
+        {'label': 'Incidents', 'value': '##### Lise, you can pick your value here'},
+        {'label': 'Fatal incidents', 'value': '##### Lise, you can pick your value here'},
+        {'label': 'Fatalities', 'value': '##### Lise, you can pick your value here'},
+        {'label': 'Lethality', 'value': '##### Lise, you can pick your value here'},
+
+        ],
+        value='##### Lise, this is the default value for drop dowm',
+        style=dict(width='45%',
+                verticalAlign="middle")
+                ),
+
+        html.Iframe(
+                sandbox='allow-scripts',
+                id='plot1',
+                height='1000',
+                width='1500',
+                style={'border-width': '0'},
+                
+                # srcDoc =  # Lise's first function call goes here, please to add .to_html() in the end
+                
+                ),
+
+        ])
+
+##### maybeuseful for James
+
+# @app.callback(
+
+
+# dash.dependencies.Output('plot1', 'srcDoc'),
+# [dash.dependencies.Input('dd-chart1', 'value')])
+
+# def update_plot(job):
+#     '''
+#     Takes in an job_to_choose and calls make_plot to update our Altair figure
+#     '''
+#     updated_plot = trend(job_to_choose=job).to_html()
+#     return updated_plot
+#####
+    
+if __name__ == '__main__':
+    app.run_server(debug=True)
